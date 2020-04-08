2ddb81de502a5b838814cbd134d699eb9dc0eff8
Yannick Biheul <yannickbiheul@outlook.fr>
2020-04-08 20:00:17 +0200

    Préparation des variables

--- quiz.js
+++ quiz.js
@@ -1,2 +1,13 @@
 /* window.location.reload() : Recharger la page */
 
+let bouton = document.querySelector(".bouton");
+
+let score = document.querySelector("#score");
+let playerName = document.querySelector("#playerName");
+
+
+let question = document.querySelector("#question");
+let reponse1 = document.querySelector("#reponse1");
+let reponse2 = document.querySelector("#reponse2");
+let reponse3 = document.querySelector("#reponse3");
+let reponse4 = document.querySelector("#reponse4");
